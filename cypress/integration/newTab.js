dee696a85ab97472ca598fb83c22a59d4218e851
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-10-02 16:45:16 +0400

    Update newTab.js

--- cypress/integration/newTab.js
+++ cypress/integration/newTab.js
@@ -1,6 +1,6 @@
 describe(' Open link in same tab',()=>
 {});
-    it.only('loads the about page', () => {
+    it.only('loads the page using approach1', () => {
     cy.visit('https://test.io/coverage/website-testing')
     cy.get('a.cta-button__button.button-cta--get-a-demo').first()
     .invoke('attr','target','_self').click({force:true})
@@ -8,7 +8,7 @@ describe(' Open link in same tab',()=>
   
   });
 
-  it.only('loads the about page2 ', () => {
+  it.only('loads the page using approach2', () => {
     cy.visit('https://test.io/coverage/website-testing')
     cy.get('button#onetrust-accept-btn-handler').click()    
     cy.get('a.cta-button__button.button-cta--get-a-demo').first()
