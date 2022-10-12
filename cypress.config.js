83e514698042e713350e82aa79f9a0956e688e2d
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-10-12 19:32:02 +0400

    Link Test Case

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,20 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
+   
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    experimentalWebKitSupport: true
+    experimentalWebKitSupport: true,
+    
+      "retries": {
+       
+        "runMode": 2,
+       
+        "openMode": 1
+      }
+    
+    
 
   },
 })
