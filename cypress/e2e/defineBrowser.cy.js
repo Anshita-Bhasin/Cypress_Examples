5f44eda0a33f2f3878449e3d20d2fc076d228532
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-10-25 23:27:57 +0400

    find Text

--- cypress/e2e/defineBrowser.cy.js
+++ cypress/e2e/defineBrowser.cy.js
@@ -5,21 +5,15 @@ describe('Change browser in the test case', () => {
     })
 
     it("Browser is chrome", { browser: "chrome" }, () => {
-
         cy.log("Chrome");
-
     })
 
 
     it("Browser is webkit", { browser: "Electron" }, () => {
         cy.log("Webkit");
-
-
     })
 
     it("Browser is Electron", { browser: "Webkit" }, () => {
-
         cy.log("Electron");
-
     })
 })
