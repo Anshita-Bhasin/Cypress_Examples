d8d5a3577e4d5afa6784acb24cbe26ab51af118c
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-10-26 00:01:16 +0400

    Update

--- cypress/e2e/findText.cy.js
+++ cypress/e2e/findText.cy.js
@@ -2,34 +2,34 @@ let getText;
 describe("find text of an element", () => {
 
     it("Alias First Test - Using Invoke Command ", function () {
-        cy.visit("https://www.dropbox.com/")
-        cy.get('div.dwg-box.dwg-mt--1_5').invoke('text').as('textFunction');
+        cy.visit("https://www.linkedin.com/")
+        cy.get('[data-test-id="hero__headline"]').invoke('text').as('textFunction');
 
     });
 
 
-    it("Print Value - ALias => Invoke - Command)", function () {
+    it("Print Value - ALias => Invoke - Command ", function () {
         cy.log("===== Print Value Using Invoke Command ==== ", this.textFunction)
     })
 
 
     it("Alias Second Test - Using Wrap Command", () => {
-        cy.visit("https://www.dropbox.com/")
-        cy.get('div.dwg-box.dwg-mt--1_5').then($value => {
+        cy.visit("https://www.linkedin.com/")
+        cy.get('[data-test-id="hero__headline"]').then($value => {
             const textValue = $value.text()
             cy.wrap(textValue).as('wrapValue')
         })
     })
 
 
-    it("Print Value - ALias => Wrap - Command)", function () {
+    it("Print Value - ALias => Wrap - Command ", function () {
         cy.log("===== Print Value Uisng Wrap Command ==== ", this.wrapValue)
     })
 
 
     it("Third Test - using variable ", () => {
-        cy.visit("https://www.dropbox.com/")
-        cy.get('div.dwg-box.dwg-mt--1_5').then(($value) => {
+        cy.visit("https://www.linkedin.com/")
+        cy.get('[data-test-id="hero__headline"]').then(($value) => {
             getText = $value.text()
         })
     })
