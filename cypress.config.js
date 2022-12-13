9aef2cd26bc968638e6c485e8e3992b9de244e22
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-12-13 12:52:23 +0400

    Cypress Commands

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,24 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+      require("./cypress/plugins/index.js")(on, config);
     },
     experimentalWebKitSupport: true,
-    "retries": {
-      "runMode": 2,
-      "openMode": 1
+
+    retries: {
+      runMode: 2,
+      openMode: 1,
     },
     watchForFileChanges: false,
     viewportWidth: 1000,
-    viewportHeight: 600
+    viewportHeight: 600,
   },
-})
+});
