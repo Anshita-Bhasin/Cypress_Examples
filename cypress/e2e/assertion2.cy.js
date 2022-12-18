e28010dfabe3e4768c0cc1576f2eab664be83071
Anshita Bhasin <anshita.bhasin@gmail.com>
2022-12-18 22:39:57 +0400

    Assertions2

--- cypress/e2e/assertion2.cy.js
+++ cypress/e2e/assertion2.cy.js
@@ -1,5 +1,5 @@
-describe("Should Exist Assertion - If Else Condition", () => {
-  it("Open website and enter username, password", () => {
+describe("Assertion Use Case2 - If Else Condition", () => {
+  it("Condition 1- Click If the element exist on the page", () => {
     cy.visit(
       "https://naveenautomationlabs.com/opencart/index.php?route=account/login"
     );
@@ -8,11 +8,22 @@ describe("Should Exist Assertion - If Else Condition", () => {
     cy.get('[id="input-password"]').type("Cypress123!!");
 
     cy.get(".btn.btn-primary").then(($value) => {
-      if ($value.length < 0) {
+      if ($value.length > 1) {
         cy.log(" Button Exist ");
         $value.click();
       } else {
         cy.log(" Button does not exist");
+      }
+    });
+  });
+
+  it("Condition 2- If there are more than 1 elements on the page", () => {
+    cy.get(".dropdown-toggle").then(($value) => {
+      if ($value.length > 1) {
+        cy.log(" Inside If condition ");
+        $value.first().click();
+      } else {
+        cy.log(" Inside else condition ");
       }
     });
   });
